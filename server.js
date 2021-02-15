bd430b4c52c8efb3a39b5bb24f2e787135bde033
Ankit Kumar <myselfankit51@gmail.com>
2021-02-15 20:38:48 +0530

    heroku deployed

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
