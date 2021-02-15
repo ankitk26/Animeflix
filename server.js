de9e0e09934c26fd3c6361f389a3a0fbe9fe7e2e
Ankit Kumar <myselfankit51@gmail.com>
2021-02-15 19:59:37 +0530

    final changes done

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(
   })
 );
 
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
