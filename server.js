56f6501bcd6f6b94596df9db094bf69bb66b3c04
Ankit Kumar <myselfankit51@gmail.com>
2021-02-15 20:30:36 +0530

    heroku deployed

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
