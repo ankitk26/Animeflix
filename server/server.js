66b5e9c8d4e0f636fd81da7fa4d49ac0385c0e5b
Ankit Kumar <myselfankit51@gmail.com>
2021-02-20 19:12:45 +0530

    issue in server fixed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schema_and_resolvers");
 const path = require("path");
 const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -11,7 +12,7 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
