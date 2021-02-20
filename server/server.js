eb63e359936183020296b54505056dbaeef3e548
Ankit Kumar <myselfankit51@gmail.com>
2021-02-20 18:58:14 +0530

    updated package.json

--- server/server.js
+++ server/server.js
@@ -10,10 +10,13 @@ app.use(cors());
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
-app.use(express.static(path.join(__dirname, "..", "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"));
+  });
+}
 
 // Listen to server
 const PORT = process.env.port || 5000;
