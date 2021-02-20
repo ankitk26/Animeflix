f37da8a06f68ff06d6983af32fb48f8638f385cf
Ankit Kumar <myselfankit51@gmail.com>
2021-02-20 19:06:38 +0530

    issue in server fixed

--- server/server.js
+++ server/server.js
@@ -19,5 +19,5 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Listen to server
-const PORT = process.env.port || 5000;
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started at port ${port}`));
